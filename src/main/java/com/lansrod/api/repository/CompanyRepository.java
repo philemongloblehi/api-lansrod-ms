597f7b9ff8ad5e69b719d0275f321d24b621ce36
Philémon Globléhi <philemongloblehi@gmail.com>
2021-03-31 15:44:37 +0000

    Implementation Repository, Service and Controller of Company and Employee entities

--- src/main/java/com/lansrod/api/repository/CompanyRepository.java
+++ src/main/java/com/lansrod/api/repository/CompanyRepository.java
@@ -1,7 +1,12 @@
 package com.lansrod.api.repository;
 
+import com.lansrod.api.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author Philemon Globlehi <philemon.globlehi@gmail.com>
  */
-public class CompanyRepository {
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
--- src/main/java/com/lansrod/api/repository/EmployeeRepository.java
+++ src/main/java/com/lansrod/api/repository/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package com.lansrod.api.repository;
 
+import com.lansrod.api.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author Philemon Globlehi <philemon.globlehi@gmail.com>
  */
-public class EmployeeRepository {
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
