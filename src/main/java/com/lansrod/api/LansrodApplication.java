67b764068dcaf2de46bbc82b4ac66c7fcdcb7b93
Philémon Globléhi <philemongloblehi@gmail.com>
2021-03-31 09:39:45 +0000

    Intitial Commit

---
+++ src/main/java/com/lansrod/api/LansrodApplication.java
@@ -0,0 +1,13 @@
+package com.lansrod.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LansrodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LansrodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lansrod/api/LansrodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lansrod.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LansrodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
